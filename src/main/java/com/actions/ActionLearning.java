f5779e15371df46e21ce4bba72dfdd09680f1c0c
YunqiCui <cuiyunqi@yahoo.com>
2018-02-27 15:27:24 +1100

    Commited

---
+++ src/main/java/com/actions/ActionLearning.java
@@ -0,0 +1,51 @@
+/*
+ * Learning ActionListeners
+ */
+package com.actions;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ActionLearning extends JFrame implements ActionListener{
+
+    JPanel jp = new JPanel();
+    JButton jb1,jb2;
+
+    public static void main(String[] args) {
+        ActionLearning al = new ActionLearning();
+    }
+
+    public ActionLearning(){
+        jp = new JPanel();
+        jb1 = new JButton("Black");
+        jb2 = new JButton("Red");
+
+        jp.add(jb1,BorderLayout.NORTH);
+        jp.add(jb2,BorderLayout.SOUTH);
+        jb1.addActionListener(this);
+        jb1.setActionCommand("Black");
+        jb2.addActionListener(this);
+        jb2.setActionCommand("Red");
+
+        this.add(jp);
+        jp.setBackground(Color.RED);
+        this.setTitle("Test");
+        this.setSize(400,300);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setLocation(200,200);
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getActionCommand().equals("Black")){
+            jp.setBackground(Color.black);
+        }else if(e.getActionCommand().equals("Red")){
+            jp.setBackground(Color.red);
+            }
+
+    }
+}
